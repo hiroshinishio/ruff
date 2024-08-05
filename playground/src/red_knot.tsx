90cf86e298d9a0da35f452cf005a06fb05fb0277
Micha Reiser <micha@reiser.io>
2024-08-05 08:57:33 +0200

    Work towards red knot playground

--- playground/src/red_knot.tsx
+++ playground/src/red_knot.tsx
@@ -1,23 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Editor from "./ruff/Editor";
+import Editor from "./red_knot/Editor";
 import "./index.css";
 import { loader } from "@monaco-editor/react";
 import { setupMonaco } from "./shared/setupMonaco";
-import { restore } from "./ruff/settings";
 import { DEFAULT_PYTHON_SOURCE } from "./constants";
-import init from "./ruff/pkg";
+import init from "./red_knot/pkg";
 
 startPlayground()
-  .then(({ sourceCode, settings, ruffVersion }) => {
-    console.log("Render", sourceCode, settings, ruffVersion);
+  .then(({ sourceCode }) => {
+    console.log("Render", sourceCode);
     ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <React.StrictMode>
-        <Editor
-          initialSettings={settings}
-          initialSource={sourceCode}
-          ruffVersion={ruffVersion}
-        />
+        <Editor initialSource={sourceCode} version="0.0.0" />
       </React.StrictMode>,
     );
   })
@@ -28,33 +23,29 @@ startPlayground()
 // Run once during startup. Initializes monaco, loads the wasm file, and restores the previous editor state.
 async function startPlayground(): Promise<{
   sourceCode: string;
-  settings: string;
-  ruffVersion: string;
 }> {
   const initialized = init();
   loader.init().then(setupMonaco);
   await initialized;
 
-  try {
-    const response = await restore();
+  // try {
+  //   // const response = await restore();
+  //
+  //   const [settingsSource, pythonSource] = response ?? [
+  //     "",
+  //     DEFAULT_PYTHON_SOURCE,
+  //   ];
+  //
+  //   return {
+  //     sourceCode: pythonSource,
+  //     settings: settingsSource,
+  //     ruffVersion: "0.0.0",
+  //   };
+  // } catch (error) {
+  //   console.warn("Failed to restore editor state", error);
 
-    const [settingsSource, pythonSource] = response ?? [
-      "",
-      DEFAULT_PYTHON_SOURCE,
-    ];
-
-    return {
-      sourceCode: pythonSource,
-      settings: settingsSource,
-      ruffVersion: "0.0.0",
-    };
-  } catch (error) {
-    console.warn("Failed to restore editor state", error);
-
-    return {
-      sourceCode: DEFAULT_PYTHON_SOURCE,
-      settings: "{}",
-      ruffVersion: "0.0.0",
-    };
-  }
+  return {
+    sourceCode: DEFAULT_PYTHON_SOURCE,
+  };
+  //s  }
 }
--- playground/src/red_knot/Editor.tsx
+++ playground/src/red_knot/Editor.tsx
@@ -1,12 +1,14 @@
-import {useCallback, useDeferredValue, useMemo, useState} from "react";
-import {Panel, PanelGroup} from "react-resizable-panels";
-import {Settings, TargetVersion, Workspace} from "./pkg";
-import {ErrorMessage} from "../shared/ErrorMessage";
+import { useCallback, useDeferredValue, useMemo, useState } from "react";
+import { Panel, PanelGroup } from "react-resizable-panels";
+import { Settings, TargetVersion, Workspace, FileHandle } from "./pkg";
+import { ErrorMessage } from "../shared/ErrorMessage";
 import Header from "../shared/Header";
 import PrimarySideBar from "./PrimarySideBar";
-import {HorizontalResizeHandle} from "../shared/ResizeHandle";
+import { HorizontalResizeHandle } from "../shared/ResizeHandle";
 
-import {useTheme} from "../shared/theme";
+import { useTheme } from "../shared/theme";
+import { Files } from "./Files";
+import SourceEditor from "../ruff/SourceEditor";
 
 type Tab = "Source" | "Settings";
 
@@ -15,134 +17,146 @@ interface CheckResult {
   error: string | null;
 }
 
+type CurrentFile = {
+  handle: FileHandle;
+  name: string;
+  content: string;
+};
+
+type FileIndex = {
+  [name: string]: FileHandle | null;
+};
+
 type Props = {
   initialSource: string;
-  initialSettings: string;
   version: string;
 };
 
-export default function Editor({
-                                 initialSource,
-
-                                 version,
-                               }: Props) {
+export default function Editor({ initialSource }: Props) {
   const [workspace, setWorkspace] = useState(() => {
-    const settings = new Settings();
-    settings.target_version = TargetVersion.Py312;
+    const settings = new Settings(TargetVersion.Py312);
     return new Workspace("/", settings);
   });
 
+  const [files, setFiles] = useState<FileIndex>({});
+
+  // The revision gets incremented everytime any persisted state changes.
   const [revision, setRevision] = useState(0);
 
-  const [file, setFile] = useState(() =>
-    workspace.openFile("main.py", initialSource),
-  );
+  const [currentFile, setCurrentFile] = useState<CurrentFile>(() => {
+    const handle = workspace.openFile("main.py", initialSource);
+    return {
+      handle,
+      content: initialSource,
+      name: "main.py",
+    };
+  });
 
   const [tab, setTab] = useState<Tab>("Source");
-  const [secondaryTool, setSecondaryTool] = useState<SecondaryTool | null>(
-    () => {
-      const secondaryValue = new URLSearchParams(location.search).get(
-        "secondary",
-      );
-      if (secondaryValue == null) {
-        return null;
-      } else {
-        return parseSecondaryTool(secondaryValue);
-      }
-    },
-  );
+  // const [secondaryTool, setSecondaryTool] = useState<SecondaryTool | null>(
+  //   () => {
+  //     const secondaryValue = new URLSearchParams(location.search).get(
+  //       "secondary",
+  //     );
+  //     if (secondaryValue == null) {
+  //       return null;
+  //     } else {
+  //       return parseSecondaryTool(secondaryValue);
+  //     }
+  //   },
+  // );
 
   const [theme, setTheme] = useTheme();
 
   // Ideally this would be retrieved right from the URL... but routing without a proper
   // router is hard (there's no location changed event) and pulling in a router
   // feels overkill.
-  const handleSecondaryToolSelected = (tool: SecondaryTool | null) => {
-    if (tool === secondaryTool) {
-      tool = null;
-    }
-
-    const url = new URL(location.href);
-
-    if (tool == null) {
-      url.searchParams.delete("secondary");
-    } else {
-      url.searchParams.set("secondary", tool);
-    }
-
-    history.replaceState(null, "", url);
-
-    setSecondaryTool(tool);
-  };
+  // const handleSecondaryToolSelected = (tool: SecondaryTool | null) => {
+  //   if (tool === secondaryTool) {
+  //     tool = null;
+  //   }
+  //
+  //   const url = new URL(location.href);
+  //
+  //   if (tool == null) {
+  //     url.searchParams.delete("secondary");
+  //   } else {
+  //     url.searchParams.set("secondary", tool);
+  //   }
+  //
+  //   history.replaceState(null, "", url);
+  //
+  //   setSecondaryTool(tool);
+  // };
 
   // TODO: figure out how to do deferred
-  const deferredSource = useDeferredValue(file);
+  const deferredSource = useDeferredValue(currentFile);
 
   const checkResult: CheckResult = useMemo(() => {
-    let file = deferredSource;
+    const file = deferredSource;
 
     try {
-      const diagnostics = workspace.checkFile(file);
+      const diagnostics = workspace.checkFile(file.handle);
 
-      let secondary: SecondaryPanelResult = null;
+      // let secondary: SecondaryPanelResult = null;
 
-      try {
-        switch (secondaryTool) {
-          case "AST":
-            secondary = {
-              status: "ok",
-              content: workspace.parsed(file),
-            };
-            break;
-
-          case "Format":
-            secondary = {
-              status: "error",
-              content: "Not supported",
-            };
-            break;
-
-          case "FIR":
-            secondary = {
-              status: "error",
-              content: "Not supported",
-            };
-            break;
-
-          case "Comments":
-            secondary = {
-              status: "error",
-              content: "Not supported",
-            };
-            break;
-
-          case "Tokens":
-            secondary = {
-              status: "ok",
-              content: workspace.tokens(file),
-            };
-            break;
-        }
-      } catch (error: unknown) {
-        secondary = {
-          status: "error",
-          error: error instanceof Error ? error.message : error + "",
-        };
-      }
+      // try {
+      //   switch (secondaryTool) {
+      //     case "AST":
+      //       secondary = {
+      //         status: "ok",
+      //         content: workspace.parsed(file),
+      //       };
+      //       break;
+      //
+      //     case "Format":
+      //       secondary = {
+      //         status: "error",
+      //         content: "Not supported",
+      //       };
+      //       break;
+      //
+      //     case "FIR":
+      //       secondary = {
+      //         status: "error",
+      //         content: "Not supported",
+      //       };
+      //       break;
+      //
+      //     case "Comments":
+      //       secondary = {
+      //         status: "error",
+      //         content: "Not supported",
+      //       };
+      //       break;
+      //
+      //     case "Tokens":
+      //       secondary = {
+      //         status: "ok",
+      //         content: workspace.tokens(file),
+      //       };
+      //       break;
+      //   }
+      // } catch (error: unknown) {
+      //   secondary = {
+      //     status: "error",
+      //     error: error instanceof Error ? error.message : error + "",
+      //   };
+      // }
 
       return {
         diagnostics,
         error: null,
-        secondary,
+        // secondary,
       };
     } catch (e) {
       return {
         diagnostics: [],
         error: (e as Error).message,
-        secondary: null,
+        // secondary: null,
       };
     }
-  }, [deferredSource, secondaryTool]);
+  }, [deferredSource]);
 
   const handleShare = useCallback(() => {
     console.log("TODO");
@@ -152,7 +166,11 @@ export default function Editor({
   }, []);
 
   const handlePythonSourceChange = useCallback((pythonSource: string) => {
-    workspace.updateFile(file, pythonSource);
+    workspace.updateFile(currentFile.handle, pythonSource);
+    setCurrentFile({
+      ...currentFile,
+      content: pythonSource,
+    });
     setRevision((revision) => revision + 1);
   }, []);
 
@@ -169,6 +187,80 @@ export default function Editor({
   //   });
   // }, []);
 
+  const handleFileAdded = useCallback(
+    (name: string) => {
+      const handle = workspace.openFile(name, "");
+
+      setFiles({
+        ...files,
+        [name]: handle,
+      });
+
+      setRevision((revision) => revision + 1);
+
+      setCurrentFile({
+        handle,
+        name: name,
+        content: "",
+      });
+
+      return handle;
+    },
+    [workspace, files],
+  );
+
+  const handleFileRemoved = useCallback(
+    (name: string) => {
+      const file = files[name];
+
+      if (file != null) {
+        workspace.closeFile(file);
+
+        const newFiles = { ...files };
+        delete newFiles[name];
+
+        setFiles(newFiles);
+        setRevision((revision) => (revision += 1));
+
+        if (currentFile.handle == file) {
+          handleFileClicked(Object.keys(files)[0]);
+        }
+      }
+    },
+    [files, workspace],
+  );
+
+  const handleFileRenamed = useCallback(
+    (oldName: string, newName: string) => {
+      const oldFile = files[oldName];
+
+      if (oldFile == null) {
+        return;
+      }
+
+      const content = workspace.sourceText(oldFile);
+      handleFileRemoved(oldName);
+      const newFile = handleFileAdded(newName);
+      workspace.updateFile(newFile, content);
+    },
+    [files, workspace],
+  );
+
+  const handleFileClicked = useCallback(
+    (name: string) => {
+      const file = files[name]!;
+
+      setCurrentFile({
+        handle: file,
+        name,
+        content: workspace.sourceText(file),
+      });
+    },
+    [files, workspace],
+  );
+
+  const fileNames = useMemo(() => Object.keys(files), [files]);
+
   return (
     <main className="flex flex-col h-full bg-ayu-background dark:bg-ayu-background-dark">
       <Header
@@ -182,14 +274,19 @@ export default function Editor({
       <div className="flex flex-grow">
         {
           <PanelGroup direction="horizontal" autoSaveId="main">
-            <PrimarySideBar
-              onSelectTool={(tool) => setTab(tool)}
+            <Files
+              files={fileNames}
+              onAdd={handleFileAdded}
+              onRename={handleFileRenamed}
+              onSelected={handleFileClicked}
+              onRemove={handleFileRemoved}
               selected={tab}
             />
             <Panel id="main" order={0} className="my-2" minSize={10}>
               <SourceEditor
+                key={currentFile.handle}
                 visible={tab === "Source"}
-                source={source.pythonSource}
+                source={currentFile.content}
                 theme={theme}
                 diagnostics={checkResult.diagnostics}
                 onChange={handlePythonSourceChange}
@@ -201,27 +298,27 @@ export default function Editor({
               {/*  onChange={handleSettingsSourceChange}*/}
               {/*/>*/}
             </Panel>
-            {secondaryTool != null && (
-              <>
-                <HorizontalResizeHandle/>
-                <Panel
-                  id="secondary-panel"
-                  order={1}
-                  className={"my-2"}
-                  minSize={10}
-                >
-                  {/*<SecondaryPanel*/}
-                  {/*  theme={theme}*/}
-                  {/*  tool={secondaryTool}*/}
-                  {/*  result={checkResult.secondary}*/}
-                  {/*/>*/}
-                </Panel>
-              </>
-            )}
-            <SecondarySideBar
-              selected={secondaryTool}
-              onSelected={handleSecondaryToolSelected}
-            />
+            {/*{secondaryTool != null && (*/}
+            {/*  <>*/}
+            {/*    <HorizontalResizeHandle />*/}
+            {/*    <Panel*/}
+            {/*      id="secondary-panel"*/}
+            {/*      order={1}*/}
+            {/*      className={"my-2"}*/}
+            {/*      minSize={10}*/}
+            {/*    >*/}
+            {/*      /!*<SecondaryPanel*!/*/}
+            {/*      /!*  theme={theme}*!/*/}
+            {/*      /!*  tool={secondaryTool}*!/*/}
+            {/*      /!*  result={checkResult.secondary}*!/*/}
+            {/*      /!>*!/*/}
+            {/*    </Panel>*/}
+            {/*  </>*/}
+            {/*)}*/}
+            {/*<SecondarySideBar*/}
+            {/*  selected={secondaryTool}*/}
+            {/*  onSelected={handleSecondaryToolSelected}*/}
+            {/*/>*/}
           </PanelGroup>
         }
       </div>
