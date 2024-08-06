04cb91fd6a9547ecd642fbf6d33868a476c6abe8
Carl Meyer <carl@astral.sh>
2024-08-06 14:24:42 -0700

    [WIP] rewrote bitsets as enums

--- crates/red_knot_python_semantic/src/semantic_index/use_def/bitset.rs
+++ crates/red_knot_python_semantic/src/semantic_index/use_def/bitset.rs
@@ -2,20 +2,17 @@ use std::collections::{btree_set, BTreeSet};
 
 /// Ordered set of `usize`; bit-set for small values (up to 128 * B), BTreeSet for overflow.
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub(crate) struct BitSet<const B: usize> {
+pub(crate) enum BitSet<const B: usize> {
     /// Bit-set (in 128-bit blocks) for the first 128 * B entries.
-    blocks: [u128; B],
+    Blocks([u128; B]),
 
-    /// Overflow storage for entries beyond 128 * B.
-    overflow: BTreeSet<usize>,
+    /// Overflow beyond 128 * B.
+    Overflow(BTreeSet<usize>),
 }
 
 impl<const B: usize> Default for BitSet<B> {
     fn default() -> Self {
-        Self {
-            blocks: [0; B],
-            overflow: BTreeSet::new(),
-        }
+        Self::Blocks([0; B])
     }
 }
 
@@ -29,90 +26,115 @@ impl<const B: usize> BitSet<B> {
         bitset
     }
 
+    /// Convert from Blocks to Overflow representation.
+    fn overflow(&mut self) {
+        if matches!(self, Self::Blocks(_)) {
+            let set = BTreeSet::from_iter(self.iter());
+            *self = Self::Overflow(set);
+        }
+    }
+
     /// Insert a value into the BitSet.
     ///
     /// Return true if the value was newly inserted, false if already present.
     pub(crate) fn insert(&mut self, value: usize) -> bool {
         if value >= Self::BITS {
-            self.overflow.insert(value)
-        } else {
-            let (block, index) = (value / 128, value % 128);
-            let missing = self.blocks[block] & (1_u128 << index) == 0;
-            self.blocks[block] |= 1_u128 << index;
-            missing
+            self.overflow();
+        }
+        match self {
+            Self::Blocks(blocks) => {
+                let (block, index) = (value / 128, value % 128);
+                let missing = blocks[block] & (1_u128 << index) == 0;
+                blocks[block] |= 1_u128 << index;
+                missing
+            }
+            Self::Overflow(set) => set.insert(value),
         }
     }
 
     /// Merge another BitSet into this one.
     ///
-    /// Equivalent to (but more efficient than) iterating the other BitSet and inserting its values
-    /// one-by-one into this BitSet.
+    /// Equivalent to (but often more efficient than) iterating the other BitSet and inserting its
+    /// values one-by-one into this BitSet.
     pub(crate) fn merge(&mut self, other: &BitSet<B>) {
-        for i in 0..B {
-            self.blocks[i] |= other.blocks[i];
+        match (self, other) {
+            (Self::Blocks(myblocks), Self::Blocks(other_blocks)) => {
+                for i in 0..B {
+                    myblocks[i] |= other_blocks[i];
+                }
+            }
+            (Self::Overflow(myset), Self::Overflow(other_set)) => {
+                myset.extend(other_set);
+            }
+            (me, other) => {
+                for value in other.iter() {
+                    me.insert(value);
+                }
+            }
         }
-        self.overflow.extend(&other.overflow);
     }
 
     /// Return `true` if this BitSet contains `value`; `false` if not.
     pub(crate) fn contains(&self, value: usize) -> bool {
-        if value >= Self::BITS {
-            self.overflow.contains(&value)
-        } else {
-            let (block, index) = (value / 128, value % 128);
-            self.blocks[block] & (1_u128 << index) != 0
+        match self {
+            Self::Blocks(blocks) => {
+                let (block, index) = (value / 128, value % 128);
+                blocks[block] & (1_u128 << index) != 0
+            }
+            Self::Overflow(set) => set.contains(&value),
         }
     }
 
     /// Return an iterator over the values (in ascending order) in this BitSet.
     pub(crate) fn iter(&self) -> BitSetIterator<'_, B> {
-        BitSetIterator {
-            bitset: self,
-            cur_block_index: 0,
-            cur_block: self.blocks[0],
-            overflow_iterator: None,
+        match self {
+            Self::Blocks(blocks) => BitSetIterator::Blocks(BitSetBlocksIterator {
+                blocks: &blocks,
+                cur_block_index: 0,
+                cur_block: blocks[0],
+            }),
+            Self::Overflow(set) => BitSetIterator::Overflow(set.iter()),
         }
     }
 }
 
 /// Iterator over values in a [`BitSet`].
-pub(crate) struct BitSetIterator<'a, const B: usize> {
-    /// The [`BitSet`] we are iterating over.
-    bitset: &'a BitSet<B>,
+pub(crate) enum BitSetIterator<'a, const B: usize> {
+    Blocks(BitSetBlocksIterator<'a, B>),
+    Overflow(btree_set::Iter<'a, usize>),
+}
+
+pub(crate) struct BitSetBlocksIterator<'a, const B: usize> {
+    /// The blocks we are iterating over.
+    blocks: &'a [u128; B],
 
     /// The index of the block we are currently iterating through.
     cur_block_index: usize,
 
     /// The block we are currently iterating through (and zeroing as we go.)
     cur_block: u128,
-
-    /// An iterator through the overflow [`BTreeSet`], if
-    overflow_iterator: Option<btree_set::Iter<'a, usize>>,
 }
 
 impl<const B: usize> Iterator for BitSetIterator<'_, B> {
     type Item = usize;
 
     fn next(&mut self) -> Option<Self::Item> {
-        while self.cur_block == 0 {
-            if self.cur_block_index == B - 1 {
-                if let Some(iter) = &mut self.overflow_iterator {
-                    return iter.next().copied();
-                } else {
-                    if self.bitset.overflow.is_empty() {
+        match self {
+            Self::Blocks(iter) => {
+                while iter.cur_block == 0 {
+                    if iter.cur_block_index == B - 1 {
                         return None;
                     }
-                    self.overflow_iterator = Some(self.bitset.overflow.iter());
-                    return self.overflow_iterator.as_mut().unwrap().next().copied();
+                    iter.cur_block_index += 1;
+                    iter.cur_block = iter.blocks[iter.cur_block_index];
                 }
+                let value = iter.cur_block.trailing_zeros() as usize;
+                // reset the lowest set bit
+                iter.cur_block &= iter.cur_block.wrapping_sub(1);
+                Some(value + (128 * iter.cur_block_index))
             }
-            self.cur_block_index += 1;
-            self.cur_block = self.bitset.blocks[self.cur_block_index];
+            Self::Overflow(set_iter) => set_iter.next().copied(),
         }
-        let value = self.cur_block.trailing_zeros() as usize;
-        // reset the lowest set bit
-        self.cur_block &= self.cur_block.wrapping_sub(1);
-        Some(value + (128 * self.cur_block_index))
     }
 }
 
@@ -120,22 +142,22 @@ impl<const B: usize> std::iter::FusedIterator for BitSetIterator<'_, B> {}
 
 /// Array of BitSet<B>. Up to N stored inline, more than that in overflow vector.
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub(crate) struct BitSetArray<const B: usize, const N: usize> {
-    /// Array of first N BitSets.
-    array: [BitSet<B>; N],
+pub(crate) enum BitSetArray<const B: usize, const N: usize> {
+    Array {
+        /// Array of N BitSets.
+        array: [BitSet<B>; N],
 
-    /// Overflow storage for BitSets beyond N.
-    overflow: Vec<BitSet<B>>,
+        /// How many of the bitsets are used?
+        size: usize,
+    },
 
-    /// How many of the bitsets are used?
-    size: usize,
+    Overflow(Vec<BitSet<B>>),
 }
 
 impl<const B: usize, const N: usize> Default for BitSetArray<B, N> {
     fn default() -> Self {
-        Self {
+        Self::Array {
             array: std::array::from_fn(|_| BitSet::default()),
-            overflow: vec![],
             size: 0,
         }
     }
@@ -151,88 +173,87 @@ impl<const B: usize, const N: usize> BitSetArray<B, N> {
         array
     }
 
-    /// Push an empty [`BitSet`] onto the end of the array and return a reference to it.
-    pub(crate) fn push_back(&mut self) -> &BitSet<B> {
-        self.size += 1;
-        if self.size > N {
-            self.overflow.push(BitSet::<B>::default());
-            self.overflow.last().unwrap()
-        } else {
-            &self.array[self.size - 1]
+    fn overflow(&mut self) {
+        match self {
+            Self::Array { array, size } => {
+                let mut vec: Vec<BitSet<B>> = vec![];
+                for i in 0..(*size - 1) {
+                    vec.push(array[i].clone());
+                }
+                *self = Self::Overflow(vec);
+            }
+            Self::Overflow(_) => {}
         }
     }
 
-    /// Push an empty [`BitSet`] onto the front of the array and return a reference to it.
-    pub(crate) fn push_front(&mut self) -> &BitSet<B> {
-        self.size += 1;
-        if self.size > N {
-            self.overflow.push(BitSet::<B>::default());
-            self.overflow.last().unwrap()
-        } else {
-            &self.array[self.size - 1]
+    /// Push an empty [`BitSet`] onto the end of the array.
+    pub(crate) fn push_back(&mut self) {
+        match self {
+            Self::Array { array: _, size } => {
+                *size += 1;
+                if *size > N {
+                    self.overflow();
+                    self.push_back();
+                }
+            }
+            Self::Overflow(vec) => vec.push(BitSet::default()),
         }
     }
 
     /// Insert `value` into every [`BitSet`] in this [`BitSetArray`].
     pub(crate) fn insert_in_each(&mut self, value: usize) {
-        let mut inserted = 0;
-        for bitset in &mut self.array {
-            if inserted >= self.size {
-                return;
+        match self {
+            Self::Array { array, size } => {
+                for i in 0..*size {
+                    array[i].insert(value);
+                }
             }
-            bitset.insert(value);
-            inserted += 1;
-        }
-        for bitset in &mut self.overflow {
-            bitset.insert(value);
+            Self::Overflow(vec) => {
+                for bitset in vec {
+                    bitset.insert(value);
+                }
+            }
         }
     }
 
     /// Return an iterator over each [`BitSet`] in this [`BitSetArray`].
     pub(crate) fn iter(&self) -> BitSetArrayIterator<'_, B, N> {
-        BitSetArrayIterator {
-            array: self,
-            wrapped: self.array.iter(),
-            in_overflow: false,
-            yielded: 0,
+        match self {
+            Self::Array { array, size } => BitSetArrayIterator::Array {
+                array,
+                index: 0,
+                size: *size,
+            },
+            Self::Overflow(vec) => BitSetArrayIterator::Overflow(vec.iter()),
         }
     }
 }
 
 /// Iterator over a [`BitSetArray`].
-pub(crate) struct BitSetArrayIterator<'a, const B: usize, const N: usize> {
-    /// The [`BitSetArray`] we are iterating over.
-    array: &'a BitSetArray<B, N>,
+pub(crate) enum BitSetArrayIterator<'a, const B: usize, const N: usize> {
+    Array {
+        array: &'a [BitSet<B>; N],
+        index: usize,
+        size: usize,
+    },
 
-    /// Internal iterator over either the inline array or the overflow vector.
-    wrapped: core::slice::Iter<'a, BitSet<B>>,
-
-    /// `true` if we are now iterating over the overflow vector, otherwise `false`.
-    in_overflow: bool,
-
-    /// The number of [`BitSet`] we have yielded so far.
-    yielded: usize,
+    Overflow(core::slice::Iter<'a, BitSet<B>>),
 }
 
 impl<'a, const B: usize, const N: usize> Iterator for BitSetArrayIterator<'a, B, N> {
     type Item = &'a BitSet<B>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.yielded >= self.array.size {
-            return None;
-        }
-        self.yielded += 1;
-        if self.in_overflow {
-            self.wrapped.next()
-        } else {
-            let ret = self.wrapped.next();
-            if let Some(val) = ret {
-                Some(val)
-            } else {
-                self.wrapped = self.array.overflow.iter();
-                self.in_overflow = true;
-                self.wrapped.next()
+        match self {
+            Self::Array { array, index, size } => {
+                if *index >= *size {
+                    return None;
+                }
+                let ret = Some(&array[*index]);
+                *index += 1;
+                ret
             }
+            Self::Overflow(iter) => iter.next(),
         }
     }
 }
@@ -255,6 +276,7 @@ mod tests {
             let mut b = BitSet::<1>::with(3);
             b.insert(27);
             b.insert(6);
+            assert!(matches!(b, BitSet::Blocks(_)));
             assert_bitset(&b, &[3, 6, 27]);
         }
 
@@ -263,10 +285,7 @@ mod tests {
             let mut b = BitSet::<1>::with(140);
             b.insert(100);
             b.insert(129);
-            assert_eq!(
-                b.overflow.iter().copied().collect::<Vec<usize>>(),
-                vec![129, 140]
-            );
+            assert!(matches!(b, BitSet::Overflow(_)));
             assert_bitset(&b, &[100, 129, 140]);
         }
 
@@ -285,12 +304,8 @@ mod tests {
         fn multiple_blocks() {
             let mut b = BitSet::<2>::with(130);
             b.insert(45);
-            b.insert(280);
-            assert_eq!(
-                b.overflow.iter().copied().collect::<Vec<usize>>(),
-                vec![280]
-            );
-            assert_bitset(&b, &[45, 130, 280]);
+            assert!(matches!(b, BitSet::Blocks(_)));
+            assert_bitset(&b, &[45, 130]);
         }
 
         #[test]
@@ -334,7 +349,10 @@ mod tests {
             ba.insert_in_each(79);
             assert_array(&ba, &[vec![3, 79], vec![79]]);
 
+            assert!(matches!(ba, BitSetArray::Array { .. }));
+
             ba.push_back();
+            assert!(matches!(ba, BitSetArray::Overflow(_)));
             assert_array(&ba, &[vec![3, 79], vec![79], vec![]]);
 
             ba.insert_in_each(130);
